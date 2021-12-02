b700b0ac6e9ba27df003083c4f400f16d51319d3
MindHardt <71509337+MindHardt@users.noreply.github.com>
2021-12-02 18:32:26 +0500

    Removed unused code

--- Algorithms Lab 5 - Graphs/Program.cs
+++ Algorithms Lab 5 - Graphs/Program.cs
@@ -7,10 +7,10 @@ namespace Graphs
     {
         static void Main(string[] args)
         {
-            string pathFolder = @"C:\Users\igorb\Desktop\Graph";
-            Graph graph = Graph.FromCSV(File.ReadAllLines(pathFolder + "\\graph.csv"), '\t');
-            graph.SaveCSV(pathFolder + "\\graph1.csv");
-            graph.SaveJSON(pathFolder + "\\graph1.json");
+            //string pathFolder = @"C:\Users\igorb\Desktop\Graph";
+            //Graph graph = Graph.FromCSV(File.ReadAllLines(pathFolder + "\\graph.csv"), '\t');
+            //graph.SaveCSV(pathFolder + "\\graph1.csv");
+            //graph.SaveJSON(pathFolder + "\\graph1.json");
         }
     }
 }
